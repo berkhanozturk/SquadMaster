7182e017eaf86cbb5673afacd2e2696354e816c9
Berkhan Öztürk <berkhanozturk@etu.edu.tr>
2025-03-24 15:34:32 +0300

    Update PlayerTest.java
    Package konumu düzeltildi. Ekstra import kısmında ufak değişiklikler yapıldı

--- SquadMaster/src/test/java/org/example/PlayerTest.java
+++ SquadMaster/src/test/java/org/example/PlayerTest.java
@@ -1,8 +1,8 @@
+package org.example;
 import org.example.Model.Player;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerTest {
 
